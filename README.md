68b8e378939b77a168248fed85d7ed89e312cfee
meixuannet <postmaster@webdriver.top>
2018-10-17 17:26:50 +0800

    first upload sdk

---
+++ README.md
@@ -0,0 +1,72 @@
+# webdriver-jssdk v0.0.0
+
+# 介绍
+
+本项目是webdriver的基于nodejs的jssdk库。
+
+# 安装
+
+```
+npm install webdriver-jssdk --save
+```
+
+# 使用
+
+```
+var webdriver = require('webdriver-jssdk')
+
+// 配置参数
+var options = {
+    appid: '',
+    accesskey: '',
+    secretkey: '',
+    noticeCallback: noticeCallback,
+    connectStateCallback: connectStateCallback
+}
+// 开始连接
+webdriver.connect(options)
+```
+
+# 接口
+
+## connect
+
+```
+webdriver.connect(options)
+```
+
+## disconnect
+
+```
+webdriver.disconnect(appid)
+```
+
+## write
+
+```
+webdriver.write(appid, pageid, wid, name, sid, value)
+```
+
+## showPage
+
+```
+webdriver.showPage(appid, curpageid, targetpageid, sid)
+```
+
+## showMessage
+
+```
+webdriver.showMessage(appid, pageid, msgtype, title, message, duration, sid)
+```
+
+# 链接
+
+[帮助中心](http://help.webdriver.top/guide/js.html)
+
+# 作者
+
+[webdriver](http://www.webdriver.top)
+
+# 版权
+
+Copyright (c) 2018 MeiXuan, [www.webdriver.top](http://www.webdriver.top)
---
+++ main/api.js
@@ -0,0 +1,211 @@
+var _ = require('lodash');
+var mqtt = require('mqtt');
+var crypto = require('crypto');
+
+const MQTT_SSL = true;
+const MQTT_PORT = 8103;
+const MQTT_PORT_S = 8104;
+
+function api(){
+    this.clients = [
+        // {
+        //     serverip: '',
+        //     appid: '',
+        //     accesskey: '',
+        //     secretkey: '',
+        //     mqttclient: null,
+        //     mqttstate: false,
+        //     notice_callback: null,
+        //     connectstate_callback: null
+        // }
+    ];
+}
+module.exports = new api();
+
+api.prototype.makeSign = function(appid, accesskey, secretkey, strnow){
+    var md5 = crypto.createHash("md5");    
+    md5.update(appid + accesskey + secretkey + strnow);
+    var str = md5.digest('hex');
+    var res = str.toUpperCase();  //32位大写
+    return res
+}
+
+
+//连接应用
+api.prototype.connect = function(options) {
+    if (!options || !options.appid || options.accesskey || options.secretkey) {
+        throw new Error('[webdriver-jssdk] error options');
+    }
+
+    var serverip = options.serverip || '';
+    var appid = options.appid;
+    var accesskey = options.accesskey;
+    var secretkey = options.secretkey;
+    var noticeCallback = options.noticeCallback;
+    var connectStateCallback = options.connectStateCallback;
+
+    var t = this;
+    t.disconnect(appid)
+    var client = {
+        serverip: serverip,
+        appid: appid,
+        accesskey: accesskey,
+        secretkey: secretkey,
+        mqttclient: null,
+        mqttstate: false,
+        notice_callback: noticeCallback,
+        connectstate_callback: connectStateCallback
+    }
+    t.clients.push(client)
+    
+    var strnow = Date.now()
+    var sign = t.makeSign(appid, accesskey, secretkey, strnow)
+
+    var options = {
+        username: accesskey,
+        password: sign,
+        keepalive: 60,
+        // protocolId: 'MQTT',
+        // protocolVersion: 4,
+        reconnectPeriod: 5000,
+        connectTimeout: 30 * 1000,
+        clean: true,
+        clientId: appid + "|md5|" + strnow
+    };
+    var ip = 'mqtt.webdriver.top'
+    if(serverip !== ''){
+        ip = serverip
+    }
+    var mqtt_server_url = 'mqtt://' + ip + ':' + MQTT_PORT;
+    if (MQTT_SSL) {
+        mqtt_server_url = 'tls://' + ip + ':' + MQTT_PORT_S;
+        options.rejectUnauthorized = false
+    }
+    client.mqttclient = mqtt.connect(mqtt_server_url, options);
+    if(client.mqttclient != null){
+        client.mqttclient.on('connect', function () {
+            client.mqttstate = true;
+            var topic = ['v1', appid, '+', '+', '+', '+', 'notice'].join('/'); 
+            client.mqttclient.subscribe(topic);
+            if(client.connectstate_callback){
+                client.connectstate_callback(client.appid, true)
+            }
+        });
+        //开始重联时触发
+        client.mqttclient.on('reconnect', function(){
+            client.mqttstate = false;
+        });
+        //连接断开时触发
+        client.mqttclient.on('close', function(){
+            client.mqttstate = false;
+            if(client.connectstate_callback){
+                client.connectstate_callback(client.appid, false)
+            }
+        });
+        //掉线时触发
+        client.mqttclient.on('offline', function(){
+            client.mqttstate = false;
+            if(client.connectstate_callback){
+                client.connectstate_callback(client.appid, false)
+            }
+        });
+        //连接失败或解析错误时触发
+        client.mqttclient.on('error', function(){
+            client.mqttstate = false;
+            if(client.connectstate_callback){
+                client.connectstate_callback(client.appid, false)
+            }
+        });
+        client.mqttclient.on('message', function (topic, message) {
+            var strtopic = topic.toString();
+            if (strtopic.indexOf('/') < 0) return;
+            var tps = strtopic.split('/');
+            if(tps == null)  return;
+            var len = tps.length;
+            if(len !== 7)  return;
+            var lastitem = tps[len - 1];
+            
+            if(lastitem == "notice"){
+                var topic_parsed = {
+                    ver: tps[0],
+                    appid: tps[1],
+                    pageid: tps[2],
+                    wid: tps[3],
+                    name: tps[4],
+                    sid: tps[5],
+                    cmd: tps[6],
+                }
+                if(client.notice_callback){
+                    client.notice_callback(
+                        topic_parsed.appid,
+                        topic_parsed.pageid,
+                        topic_parsed.wid,
+                        topic_parsed.name,
+                        topic_parsed.sid,
+                        message.toString()
+                    );
+                }
+            }
+        });
+    }
+}
+
+//断开应用连接
+api.prototype.disconnect = function(appid){
+    var t = this
+    var client = _.find(t.clients, function(c){
+        return c.appid == appid
+    })
+    if(client){
+        if(client.mqttclient){
+            client.mqttclient.close();
+            client.mqttclient = null;
+        }
+        client.mqttstate = false;
+    }
+    t.clients = t.clients.filter(function(c){
+        return c.appid != appid
+    })
+}
+
+//向页面发送消息提示
+api.prototype.sendMessage = function(appid, pageid, type, title, message, duration, sid){
+    var t = this
+    var client = _.find(t.clients, function(c){
+        return c.appid == appid
+    })
+    if(client && client.mqttclient && client.mqttstate){
+        var topic = ['v1', appid, pageid, '0', 'message', sid, 'write'].join('/');
+        var payload = type + "|" + title + "|" + message + "|" + duration
+        client.mqttclient.publish(topic, payload);
+    }
+}
+
+//写页面组件变量
+api.prototype.write = function(appid, pageid, wid, name, sid, value){
+    var t = this
+    var client = _.find(t.clients, function(c){
+        return c.appid == appid
+    })
+    if(client && client.mqttclient && client.mqttstate){
+        var topic = ['v1', appid, pageid, wid, name, sid, 'write'].join('/');
+        var strpayload = value
+
+        if (typeof value !== 'string') {
+            strpayload = JSON.stringify(value)
+        }
+        client.mqttclient.publish(topic, strpayload);
+    }
+}
+
+//显示指定页面
+api.prototype.showPage = function(appid, pageid, despageid, sid){
+    var t = this
+    var client = _.find(t.clients, function(c){
+        return c.appid == appid
+    })
+    if(client && client.mqttclient && client.mqttstate && pageid && despageid){
+        var topic = ['v1', appid, pageid, despageid, 'showpage', sid, 'write'].join('/');
+        client.mqttclient.publish(topic, despageid);
+    }
+}
